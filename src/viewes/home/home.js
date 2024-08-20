f28fce6a14a29a26b77ef610e125dd80ed9c29a9
alihaddad <ALIHADDAD.8989@GMAIL.COM>
2024-08-20 18:05:14 +0200

    start

---
+++ src/viewes/home/home.js
@@ -0,0 +1,33 @@
+import './home.css';
+
+const Home = () => {
+    return (
+        
+        <div className="body-home">
+            <div className='main-content'>
+                <div className='header'>
+                    <h1>header</h1>
+                </div>
+{}                <div className='outlet'>
+                    <h1>outlt</h1>
+                </div>
+                <div className='footer'>
+                 <h1>footer</h1>
+                </div>
+                
+            </div>
+            <div className='seid-bar'>
+                <div className='seidbar-brand'>
+                  
+                </div> 
+                <div className='sidebar-menu'>
+
+                </div>
+            
+           
+            </div>
+            
+         </div>
+     )
+}
+export default Home;
